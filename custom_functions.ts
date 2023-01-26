7952c3b7669938ae24000418c8f5fc0a7e07fcec
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-26 10:00:33 +0100

    Update custom_functions.ts, t_level1.md

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,8 +1,26 @@
-enum CartType {
+enum Cart {
     //% block="Lege minecart"
     EmptyCart,
     //% block="Redstone minecart"
     RedstoneCart,
+}
+
+enum Lever {
+    //% block="1"
+    Lever1,
+    //% block="2"
+    Lever2,
+    //% block="3"
+    Lever3,
+    //% block="4"
+    Lever4,
+}
+
+enum LeverStatus {
+    //% block="aan"
+    On,
+    //% block="uit"
+    Off,
 }
 
 //% color=190 weight=100 block="CodeCosmos"
@@ -16,8 +34,40 @@ namespace CodeCosmos {
     }
 
     //% block="detecteer minecart $type"
-    export function detectCart(type:CartType) {
+    export function detectCart(type:Cart) {
         return typeOfCart === type
+    }
+
+    //% block="zet schakelaar $lever $status"
+    export function setLever(lever:Lever, status:LeverStatus) {
+        const detectPositions = [world(173, 44, 327), world(161, 44, 327), world(173, 44, 323), world(161, 44, 323)]
+        player.say(`lever: ${lever}; status: ${status}`)
+
+
+        if(blocks.testForBlock(Block.GreenWool, detectPositions[lever]) !== !!(status === LeverStatus.On)) {
+            blocks.place(status === LeverStatus.On ? Block.GreenWool : Block.RedConcrete, detectPositions[lever])
+            if(lever === Lever.Lever1) {
+                agent.teleport(world(173,44,327),CompassDirection.West)
+                agent.interact(FourDirection.Forward)
+                return;
+            }
+            if (lever === Lever.Lever2) {
+                agent.teleport(world(161, 44, 327), CompassDirection.East)
+                agent.interact(FourDirection.Forward)
+                return;
+            }
+            if (lever === Lever.Lever3) {
+                agent.teleport(world(173, 44, 323), CompassDirection.West)
+                agent.interact(FourDirection.Forward)
+                return;
+            }
+            if (lever === Lever.Lever4) {
+                agent.teleport(world(161, 44, 323), CompassDirection.East)
+                agent.interact(FourDirection.Forward)
+                return;
+            }
+        }
+        
     }
 
 }
